c7ff9b498792f52e5000b281f6d33c123f3fe27c
LivePwndz <pwndz172@gmail.com>
2015-09-05 13:03:52 +0300

    adding application/config/database.php

---
+++ application/logs/2015-09-05.log.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2015-09-05 15:16:43 +05:30 --- error: Uncaught PHP Error: mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead in file A:/C_CLONE/wamp/www/zmart/system/libraries/drivers/Database/Mysql.php on line 61
