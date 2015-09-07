c97c13e0911d021139d19c1f39301ddf1c490f0d
Mustapha <Mustapha@192.168.1.134>
2015-09-07 10:35:57 +0100

    Init Code

---
+++ application/logs/2015-09-07.log.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2015-09-07 14:51:20 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file system/core/Kohana.php on line 1162
+2015-09-07 14:51:21 +05:30 --- error: Uncaught Kohana_Exception: The requested view, _error/404, could not be found in file system/core/Kohana.php on line 1162
